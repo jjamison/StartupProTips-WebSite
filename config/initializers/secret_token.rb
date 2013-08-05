ae8f14d6707e5824c7035937298a9200fe037c30
Jay Jamison <jeremiah.s.jamison@gmail.com>
2013-08-05 10:52:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UdemyApp::Application.config.secret_token = '6ac1e58880d15a67d655496fec981bd500a326d74e4fbd2b8652f5925340b6f02e5f63f27339a3641ad7ccd284c3ad2db4f418645f318edcbc616d6b972353d8'
